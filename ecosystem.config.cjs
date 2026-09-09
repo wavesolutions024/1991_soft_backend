46745dd92000fd377d3e511bf51b230da6034eba
ketanrd26 <ketandudka566123@gmail.com>
2026-09-09 18:10:13 +0530

    feat: add WhatsApp configuration parameters to ecosystem file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,7 +15,13 @@ module.exports = {
         DATABASE:"1991_crm",
         PORT:3003,
         JWT_SECRET:"1991@jwt_secret",
-        BASE_URL:"https://backend.1991tattoos.com"
+        BASE_URL:"https://backend.1991tattoos.com",
+        EXCEL_EXPORT_PASSWORD:"clientSheet@Admin",
+        WHATSAPP_VERIFY_TOKEN:"1991@Whatsapp_token",
+        WHATSAPP_ACCESS_TOKEN:"EABADiZB08efcBSQsyCAaO9c28Caj6rZCJG3oHkRL4ZCRkJGXY85nhY7u147Yn6o5FajZAcqxpO0e4oq8mzZBu4pvZCm5HEfXneBGrm8MsMQZAfOxz3PBXhd38paZByvjxDRGrEiqG1IpcZBn2f7rANayf29dvXQ8b63mzwVZBtkzj2iXrzwUWtnZBOpDqLj8ZAZApP5WH0gZDZD",
+        WHATSAPP_BUSINESS_ACCOUNT_ID:"1117014760662587", 
+        WHATSAPP_PHONE_NUMBER_ID:"1343549445488835",
+        META_GRAPH_VERSION:"v23.0"
       },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
